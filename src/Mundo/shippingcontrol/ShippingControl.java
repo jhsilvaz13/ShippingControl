db42a124f41d446c1b344547020a453d167a5b35
Umba <eumbarila@unal.edu.co>
2021-11-20 13:48:54 -0500

    Clase corregida

--- src/Mundo/shippingcontrol/ShippingControl.java
+++ src/Mundo/shippingcontrol/ShippingControl.java
@@ -1,20 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package shippingcontrol;
+package Mundo.shippingcontrol;
 
-/**
- *
- * @author jhonz
- */
 public class ShippingControl {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         int x=0;
         int y=1;
     }
