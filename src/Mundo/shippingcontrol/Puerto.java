e484eacf0fa689bcb04c33a1fd37321b37cec66d
ssiabatto <ssiabatto@unal.edu.co>
2021-11-20 18:34:37 -0500

    Edit

--- src/Mundo/shippingcontrol/Puerto.java
+++ src/Mundo/shippingcontrol/Puerto.java
@@ -9,5 +9,20 @@ package Mundo.shippingcontrol;
  * @author snsc2
  */
 public class Puerto {
+    private String nombrePuerto;
+    private int tarifa;
+    private int contenedoresMax;
+    private int barcosMax;
+    private int contenedoresAct;
+    private int barcosAct;
     
+    public Puerto(String nombrePuerto, int tarifa, int contenedoresMax,
+            int barcosMax, int barcosAct, int contenedoresAct){
+        this.nombrePuerto = nombrePuerto;
+        this.tarifa = tarifa;
+        this.contenedoresMax = contenedoresMax;
+        this.barcosMax = barcosMax;
+        this.contenedoresAct = contenedoresAct;
+        this.barcosAct = barcosAct;
+    }
 }
